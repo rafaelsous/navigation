0400a6c177352e0f4097c0275c00bb1f4ff9c7b9
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-21 13:56:09 -0300

    Customizing Bottom Navigator

--- src/routes/BottomTabRoutes.tsx
+++ src/routes/BottomTabRoutes.tsx
@@ -5,6 +5,7 @@ import {
 
 import { Home } from "@/app/Home";
 import { Product } from "@/app/Product";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export type BottomTabRoutesList = {
   home: undefined;
@@ -22,9 +23,35 @@ const { Navigator, Screen } = createBottomTabNavigator<BottomTabRoutesList>();
 
 export function BottomTabRoutes() {
   return (
-    <Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={Home} />
-      <Screen name="product" component={Product} />
+    <Navigator
+      initialRouteName="home"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2C46B1",
+        tabBarInactiveTintColor: "#444444",
+        tabBarLabelPosition: "beside-icon",
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: "Início",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: "Produto",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
